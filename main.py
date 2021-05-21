6cb113df9e0260309f1f4e094c3ef74d7fd15994
IDK-hello <84438682+IDK-hello@users.noreply.github.com>
2021-05-21 14:17:20 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from tkinter import *
+import pyautogui
+from time import sleep
+
+root = Tk()
+root.title("spa><er")
+root.minsize(600, 600)
+
+# main exec
+
+
+def mainspam():
+    for i in range(int(rep.get())):
+        sleep(5)
+        pyautogui.typewrite(keyword.get())
+        pyautogui.press('enter')
+
+# title banner
+
+
+# textbox for keyword
+
+
+labelWord = Label(root, text="keyword : ")
+labelWord.place(relx=0.3, rely=0.3)
+keyword = Entry(root)
+keyword.place(relx=0.4, rely=0.3)
+
+# textbox for rep
+
+
+labelRep = Label(root, text="Amount : ")
+labelRep.place(relx=0.3, rely=0.4)
+rep = Entry(root)
+rep.place(relx=0.4, rely=0.4)
+
+# button to exec
+
+
+execute = Button(root, width=40, height=5, text="Click Me to Execute!", command=mainspam, fg="grey", bg="white")
+execute.place(relx=0.26, rely=0.5)
+warning = Label(root, text="Please Wait 5 Second's After you Pressed the Button", font=("times", 10))
+warning.place(relx=0.26, rely=0.7)
+
+root.mainloop()
